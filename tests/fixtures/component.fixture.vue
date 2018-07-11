681c7323cbbb4123fd0901434272a794cef4645c
Cristian Pallarés <cpallares@inbenta.com>
2018-07-11 10:54:03 +0200

    chore: upgrade dependencies

---
+++ tests/fixtures/component.fixture.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :css="cssClasses">
+        Hello {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: "world",
+        cssClasses: "heading--dark",
+    }),
+};
+</script>
