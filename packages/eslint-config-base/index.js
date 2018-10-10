6e7f9d28959513cc0e87245c701e228f74296e7d
Cristian Pallarés <cpallares@inbenta.com>
2018-10-10 11:06:57 +0200

    feat(base): upgrade eslint config

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -7,10 +7,9 @@ module.exports = {
         es6: true,
     },
     parserOptions: {
-        ecmaVersion: 2017,
+        ecmaVersion: 2018,
         sourceType: "module",
         ecmaFeatures: {
-            experimentalObjectRestSpread: true,
             impliedStrict: true,
             jsx: true,
         },
