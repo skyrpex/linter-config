0475f2c87d4efce0faa0d684760caf9318cfbb76
Cristian Pallarés <cristian@pallares.io>
2019-02-08 20:39:40 +0100

    test: fix spawning tslint on Windows

--- tests/utils.js
+++ tests/utils.js
@@ -1,5 +1,11 @@
 const path = require("path");
 const { CLIEngine } = require("eslint");
+const { spawnSync } = require("child_process");
+const which = require("npm-which")(process.cwd());
+
+module.exports.spawnNpmSync = (bin, args = []) => {
+    return spawnSync(which.sync(bin), args);
+};
 
 module.exports.cleanReport = ({ results, ...report }) => ({
     ...report,
